3d710a4f9a224d2c57985c717a8d55f428808296
Benson Lee <bensonlee5@gmail.com>
2022-02-13 14:28:26 -0800

    upd

--- python/problems/p2_task.py
+++ python/problems/p2_task.py
@@ -1,5 +1,5 @@
 if __name__ == "__main__":
-  # TODO: 2b - specify different coffee drinks and send to p2_worker to create
+  # TODO: 2a - specify different coffee drinks and send to p2_worker to create
 
   pass
   
--- python/problems/p2_worker.py
+++ python/problems/p2_worker.py
@@ -1,3 +1,4 @@
 if __name__ == "__main__":
-  # TODO: 2b - create Espresso machine object, queuing up espresso orders as delivered by p2_task
+  # TODO: 2a - create Espresso machine object, queuing up espresso orders as delivered by p2_task
+  
   pass
